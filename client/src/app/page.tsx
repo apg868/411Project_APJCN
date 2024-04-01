adf4dde3454d80d32e368e079677e001bbfe788f
FilipLe <48429871+FilipLe@users.noreply.github.com>
2024-04-01 12:20:24 -0400

    divide out into components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import BudgetChart from "./budgetChart";
+import BudgetChart from "./components/budgetChart";
 
 export default function Home() {
   // Dummy data for visualization
