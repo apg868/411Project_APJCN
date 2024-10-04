2c9dee531935ab1fb341c70fe00c1c4953f8ae16
apg868 <123202954+apg868@users.noreply.github.com>
2024-10-04 15:31:40 -0400

    modifications to db credentials

--- restapi/fdb.js
+++ restapi/fdb.js
@@ -1,16 +1,21 @@
 // using 'pg' library for PostgreSQL interaction
 const Pool = require('pg').Pool;
+require('dotenv').config();
 
 // pool provides a way to maintain open connection with
 // db instead of tearing down and rebuildling connection
 // repeatedly
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "finances",
-  password: "Cloroxdognose2!",
-  port: 5432
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 });
+
+pool.connect()
+  .then(() => console.log('Connected to PostgreSQL'))
+  .catch(err => console.error('Connection error', err.stack));
 
 // export the pool
 module.exports = pool;
